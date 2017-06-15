83b1be92ae4ea61ecdc60d338e4c63ead863ff7a
ECOMMERA0\martin.atsinov <martin.atsinov@ecommera.com>
2017-06-15 10:54:41 +0300

    no message

--- react-test/src/actions/getRepos.js
+++ react-test/src/actions/getRepos.js
@@ -1,13 +1,13 @@
 import { GET_REPOS } from './const';
 
 function action(parameter) {
-  return async (dispatch) => {
-    let repos = await fetch(`https://api.github.com/users/mapolin/repos`)
-      .then(response => response.json())
-
-    dispatch({
-      type: GET_REPOS,
-      repos: repos
+  return (dispatch) => {
+    fetch(`https://api.github.com/users/mapolin/repos`)
+    .then(response => {
+      dispatch({
+        type: GET_REPOS,
+        repos: response.json()
+      })
     })
   }
 }
